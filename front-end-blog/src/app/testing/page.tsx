973783b0ff7f10ff35df6e16d223d39b6f57e1bf
DESKTOP-20BACJI\Admin <binh.ngv.dev@gmail.com>
2023-11-21 23:25:07 +0700

    init

---
+++ front-end-blog/src/app/testing/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: 'this is for testing individual comopoent',
+    description: 'this is for testing individual comopoent',
+}
+export default function Testing(){
+    return (<>
+        <div>this is for testing individual comopoent</div>
+    </>)
+}
