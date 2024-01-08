9cf7ffa6dbb9bad2ee917b9e2e35bbb33bf47837
DESKTOP-20BACJI\Admin <binh.ngv.dev@gmail.com>
2024-01-08 18:37:34 +0700

    ok

---
+++ Core/Data/Repositories/Extension/AddRepositories.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Data.Repositories.Extension
+{
+    public static class RepositoriesExtension
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services) {
+
+            services.AddScoped<RouteRepository>();
+            return services;
+        }
+    }
+}
